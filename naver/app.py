c18f3cba72c87804411bc0169b2f003c4c576e63
enddl402 <enddl402@gmail.com>
2022-12-06 18:55:16 +0900

    저장

--- naver/app.py
+++ naver/app.py
@@ -1,8 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-d = requests.get('https://finance.naver.com/item/sise.naver?code=005930')
+d = requests.get('https://finance.naver.com/item/sise.naver?code=067160')
 
 soup = BeautifulSoup(d.content,'html.parser')
-print(soup.find_all('span', class_="tah")[5].text)
+print(soup.find_all('strong', id="_nowVal")[0].text)
 
