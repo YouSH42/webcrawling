8364c0650dd684152ed93368b82e913f04ade5f3
enddl402 <enddl402@gmail.com>
2022-12-15 23:07:32 +0900

    5-1days

--- insta/app.py
+++ insta/app.py
@@ -1,9 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+import urllib.request
 
 driver = webdriver.Chrome('chromedriver.exe')
 driver.get('https://instagram.com')
+
+time.sleep(2)
+e = driver.find_element_by_css_selector('input[name="username"]')
+e.send_keys('enddl402')
+e = driver.find_element_by_css_selector('input[name="password"]')
+e.send_keys('2022exer@@')
+e.send_keys(Keys.ENTER)
+
+time.sleep(5)
+driver.get('https://www.instagram.com/explore/tags/%EA%B7%A4/?next=%2F')
+time.sleep(5)
+e = driver.find_element_by_css_selector('._aagw').click()
+
+imgi = driver.find_elements_by_css_selector('.x13vifvy')[1].get_attribute('src')
+urllib.request.urlretrieve(imgi, '1.jpg')
 
 
 
